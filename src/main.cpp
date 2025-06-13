aaf39e46aef7c679ba0ecf5482adfef305688cb3
Jehru <jehru.e.harris@gmail.com>
2025-06-14 09:00:00 +1000

    gif

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@
 #include "../wink_frame_2.h"
 #include "../wink_frame_3.h"
 #include "../wink_frame_4.h"
+#include "../wink_frame_5.h"
+#include "../wink_frame_6.h"
+#include "../wink_frame_7.h"
+#include "../wink_frame_8.h"
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -15,7 +19,8 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 const unsigned char* winkFrames[] = {
-  wink_frame_1, wink_frame_2, wink_frame_3, wink_frame_4
+  wink_frame_1, wink_frame_2, wink_frame_3, wink_frame_4,
+  wink_frame_5, wink_frame_6, wink_frame_7, wink_frame_8
 };
 const int frameCount = sizeof(winkFrames) / sizeof(winkFrames[0]);
 
@@ -29,7 +34,10 @@ void loop() {
     display.clearDisplay();
     display.drawBitmap(0, 0, winkFrames[i], SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
     display.display();
-    delay(150);
+    delay(100);
   }
-  delay(1000);
+  delay(500);
 }
+
+// You must use C arrays for each frame, not a GIF file.
+// There is no Arduino library that can decode GIFs on a microcontroller with limited RAM/flash.
